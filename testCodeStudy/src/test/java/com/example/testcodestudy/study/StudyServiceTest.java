cc6c1421abf0f71324857d142c0ce5093784c011
jjoonyna <nwj3808@gmail.com>
2025-05-07 18:37:04 +0900

    도커 컨테이너 학습 완료

--- testCodeStudy/src/test/java/com/example/testcodestudy/study/StudyServiceTest.java
+++ testCodeStudy/src/test/java/com/example/testcodestudy/study/StudyServiceTest.java
@@ -4,6 +4,8 @@ import com.example.testcodestudy.domain.Member;
 import com.example.testcodestudy.domain.Study;
 import com.example.testcodestudy.domain.StudyStatus;
 import com.example.testcodestudy.member.MemberService;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.BDDMockito;
@@ -12,9 +14,19 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -26,26 +38,54 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
+@Slf4j
 @SpringBootTest //테스트 할 객체에 빈을 등록해주기 위해 필요
-//@ExtendWith(MockitoExtension.class) //모키토를 사용할수 있게 확장해주는 어노테이션 단 SpringBootTest 어노테이션 있을시 필요 없음
+@ExtendWith(MockitoExtension.class) //모키토를 사용할수 있게 확장해주는 어노테이션 단 SpringBootTest 어노테이션 있을시 필요 없음
 @ActiveProfiles("test")
 @Testcontainers //테스트 컨테이너 사용하기 위해 필요
+@ContextConfiguration(initializers = StudyServiceTest.ContainerPropertyInitializer.class) //Context 연결
 class StudyServiceTest {
 
     @Mock //객체를 생성해주는 어노테이션, 주로 구현체가 없는 인터페이스를 객체를 만들어 테스트할때 사용
     MemberService memberService;
 
-
     @Autowired
     StudyRepository studyRepository;
 
+    @Autowired
+    Environment enviroment;
+
+    //위와 동일
+    @Value("${container.port}")
+    int port;
+
     @Container // 컨테이너 사용한다는 어노테이션
     private static MySQLContainer mySQLContainer= new MySQLContainer().withDatabaseName("studytest");
+    //db를 직접 가져오는게 아니라 도커의 이미지를 가져와서 연결 가능
+    static GenericContainer mysqlContainer= new GenericContainer("mysql")
+            .withExposedPorts(3306)
+            .withEnv("MYSQL_DB", "studytest");
+
+            //Wait을 사용하여 해당 로그메세지, 포트, http 요청 이 있은후 사용하도록 대기
+            /*.waitingFor(Wait.forHttp("/create"))
+            .waitingFor(Wait.forLogMessage("안녕",1))
+            .waitingFor(Wait.forListeningPort());*/
+
+    @BeforeAll
+    static void beforeAll() {
+        Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(log);
+        mySQLContainer.followOutput(logConsumer);
+    }
 
     //매번 테스트 마다 레파지토리를 지운다(조금이라도 테스트를 빠르게 하기 위해)
     @BeforeEach
     void beforeEach() {
+        System.out.println(mySQLContainer.getMappedPort(3306));
+        System.out.println("===========================");
+        System.out.println(enviroment.getProperty("container.port"));
+        System.out.println(port);
         studyRepository.deleteAll();
+        System.out.println(mySQLContainer.getLogs());
     }
 
 
@@ -166,5 +206,16 @@ class StudyServiceTest {
 
 
 
+    }
+    //property 정보를 spring context에 저장
+    static class ContainerPropertyInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            mySQLContainer.start();
+            //container.port 프로퍼티 추가
+            TestPropertyValues.of("container.port=" + mySQLContainer.getMappedPort(3306))
+                    .applyTo(applicationContext.getEnvironment());
+        }
     }
 }
